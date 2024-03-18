694db07e6505d21d71fa5861d3b02fc67d78ee07
Naimul02 <naimulislum39@gmail.com>
2024-03-18 21:17:33 +0600

    Navigation bar is a work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+// import DaisyNav from "./components/DaisyNav/DaisyNav";
 
 function App() {
   return (
     <>
+      {/* <DaisyNav></DaisyNav> */}
+      <Navbar></Navbar>
       <h1 className="text-7xl bg-rose-600">Vite + React</h1>
     </>
   );
